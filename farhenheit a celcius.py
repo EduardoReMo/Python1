95f79da0205d303d7e5befe6a6d3e3d1fe00013a
EduardoReMo <108359392+EduardoReMo@users.noreply.github.com>
2022-06-28 12:00:56 -0500

    Add files via upload

---
+++ farhenheit a celcius.py
@@ -0,0 +1,4 @@
+print ("Programa para convertir Farhenheit a Celcius")
+farhenheit = float (input ("Ingresa los grados Farhenheit: "))
+celcius = (farhenheit - 32) * 5/9
+print ("Equivalen a", celcius, "grados celcius.") 
